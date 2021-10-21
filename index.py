dc420bb6cf3086559158295263bf909162ea2e43
hirotoyoshidome <yoshidome.hiroto@bizpa.co.jp>
2021-10-21 12:10:10 +0900

    added

---
+++ index.py
@@ -0,0 +1,17 @@
+def main() -> None:
+    """
+    main
+    """
+    print("do some work.")
+    print(add(1, 2))
+
+
+def add(n1: int, n2: int) -> int:
+    """
+    n1 plus n2
+    """
+    return n1 + n2
+
+
+if __name__ == "__main__":
+    main()
